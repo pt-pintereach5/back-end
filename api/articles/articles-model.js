702737cedd5c41c3cb817feb2e381aabb0db3bac
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-06-21 19:33:05 -0600

    restriced middleware and more article endpoints created and working.

--- api/articles/articles-model.js
+++ api/articles/articles-model.js
@@ -8,10 +8,18 @@ function findById(article_id) {
     return db('articles').where({article_id}).first();
 }
 
+async function create(article) {
+    const [id] = await db('articles').insert(article);
+    return findById(id);
+}
+
+
+
 function findArticleCategories(article_id) {
     return db('articles').join('articles-categories as ac', 'ac.article_id', 'articles.article_id').leftJoin('categories', 'categories.category_id', 'ac.category_id').select('category').where('articles.article_id', '=', article_id)
 }
 module.exports = {
     find,
     findById,
+    create
 }
--- api/articles/articles-router.js
+++ api/articles/articles-router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { restricted } = require('../auth/auth-middleware');
 const Articles = require('../articles/articles-model');
 
-router.get('/api/articles', async (req, res) => {
+router.get('/api/articles', restricted, async (req, res) => {
     try {
         const articles = await Articles.find();
         res.status(200).json(articles);
@@ -11,11 +12,21 @@ router.get('/api/articles', async (req, res) => {
     }
 });
 
-router.get('/api/articles/:id', async (req, res) => {
+router.get('/api/articles/:id', restricted, async (req, res) => {
     const {id} = req.params;
     try {
         const article = await Articles.findById(id);
         res.status(200).json(article);
+    } catch(err) {
+        res.json(err.message);
+    }
+});
+
+router.post('/api/articles', restricted, async (req, res) => {
+    const article = req.body;
+    try {
+        const newArticle = await Articles.create(article);
+        res.status(201).json(newArticle);
     } catch(err) {
         res.json(err.message);
     }
