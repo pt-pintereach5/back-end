91fed085bb4e23551eabc536376cc661761d5a33
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-06-22 19:43:54 -0600

    base get endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require('./api/server');
 
 const port = process.env.PORT;
 
+server.get('/', (req, res) =>{
+    res.status(200).json({ message: "server is running" });
+});
+
 server.listen(port, () => {
     console.log(`\n *** server listening on port ${port} *** \n`);
 });
