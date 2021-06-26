a4b11f7ca8c07332c045e506d92a453e951244c9
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-06-26 16:43:08 -0600

    bug fix

--- api/articles/articles-router.test.js
+++ api/articles/articles-router.test.js
@@ -4,6 +4,8 @@ const db = require('../../data/db-config');
 
 describe('articles router', () => {
     it('does not return data unless there is a valid JSON web token in the header', async () => {
+        const res = await request(server).get('/api/articles');
 
+        expect(res.body).toMatchObject({ message: 'token required' });
     });
 });
